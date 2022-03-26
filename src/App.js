e960c89c84e55bca402666ca76397f8ac17508c6
viniciusym <viniciusyamate5@gmail.com>
2022-03-26 00:55:17 +0000

    timer com minutos horas e segundos

--- src/App.js
+++ src/App.js
@@ -6,19 +6,42 @@ class App extends React.Component {
     super(props);
 
     this.state = {
+      formatedTime: '',
       time: 0,
       timerInput: '',
+      minutes: '',
+      seconds: '',
+      hours: '',
     }
   }
 
   countdown = (sec) => {
     const secondsInMs = sec * 1000;
+    this.setState({
+      time: sec,
+      formatedTime: this.formatTime(sec),
+    })
     let interval = setInterval(() => {
       this.setState((prevState) => ({
         time: prevState.time - 1
+      }), this.setState({
+        formatedTime: this.formatTime(this.state.time)
       }))
     }, 1000);
+    this.setState({
+      intervalId: interval,
+    })
     setTimeout(() => clearInterval(interval), secondsInMs)
+  }
+  
+  formatTime = (sec) => {
+    const hours = Math.floor(sec / 3600)
+    const minutes = Math.floor((sec / 60) % 60);
+    const seconds = Math.floor(sec % 60);
+
+    const formatedTime = `${hours >= 10 ? hours : `0${hours}`}:${minutes >= 10 ? minutes : `0${minutes}`}:${seconds >= 10 ? seconds : `0${seconds}`}`;
+    
+    return formatedTime;
   }
 
   handleinputChange = (event) => {
@@ -28,19 +51,24 @@ class App extends React.Component {
     });
   }
 
-  startTimer = (sec) => {
-    this.setState({
-      time: sec,
-    }, this.countdown(sec))
+  startTimer = (hours, min, sec) => {
+    const { intervalId } = this.state;
+    clearInterval(intervalId);
+    let totalTimerInSec = (Number(hours) * 3600) + (Number(min) * 60) + Number(sec);
+    this.countdown(totalTimerInSec)
   }
 
   render() {
-    const { time, timerInput } = this.state;
+    const { time, formatedTime, minutes, seconds, hours, intervalId } = this.state;
     return (
       <div>
-        <Timer time={ time } />
-        <input type="text" name="timerInput" onChange={(event) => this.handleinputChange(event)}/>
-        <button onClick={() => this.startTimer(timerInput)}>Start</button>
+        <Timer time={ time === 0 ? '00:00:00' : formatedTime } />
+        <input type="number" name="hours" onChange={(event) => this.handleinputChange(event)}/>
+        <input type="number" name="minutes" onChange={(event) => this.handleinputChange(event)}/>
+        <input type="number" name="seconds" onChange={(event) => this.handleinputChange(event)}/>
+        <button onClick={() => this.startTimer(hours, minutes, seconds)}>Start</button>
+        <button onClick={() => clearInterval(intervalId)}>Pause</button>
+        <button onClick={() => this.countdown(time + 1)}>Resume</button>
       </div>
     );
   }
